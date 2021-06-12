f938719667a9fc7194c67eee60d6fc4d7c9da7d0
park-jian <masichyun77@hanmail.net>
2021-06-12 23:59:34 +0900

    2021.06.12 백준 4344번문제

--- src/com/jian/test01/Main4344.java
+++ src/com/jian/test01/Main4344.java
@@ -7,33 +7,26 @@ public class Main4344 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int C = sc.nextInt();     //3
-		int N = 0;
 		int[] arr ;
-		float sum =0;
-		int stud = 0;
+		
 		for(int i=0; i < C; i++) {        //0.1.2
+			int N = 0;
+			int stud = 0;
+			float sum =0;
 			N = sc.nextInt();
 			arr = new int[N];
 			for(int j=0; j < N; j++) {
 				arr[j]= sc.nextInt();
-				sum += arr[j];
-				
+				sum += arr[j];	
 			}
-			//평균
-			System.out.print(sum/N);
-			
 			 for(int k=0; k < N; k++) {
 				 if(arr[k] > (sum/N)) {
 					 stud++;
-				 }
-			 
+				 }		 
 			 }
-			 System.out.println(String.format("%.3f", (double)(stud/N)*100));
-			 
-			 
+			 System.out.println(String.format("%.3f", ((double)stud/N)*100)+"%");
 		}
 		sc.close();
-
 	}
 
 }
